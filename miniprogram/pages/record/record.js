c6afedfbd2ed4620f0f85b82771349e178d79049
Yyy-j <yyytororo@gmail.com>
2026-05-19 13:03:50 +0900

    step3

--- miniprogram/pages/record/record.js
+++ miniprogram/pages/record/record.js
@@ -1,17 +1,57 @@
 // pages/record/record.js
 Page({
   data: {
-    meals: []
+    state: 'idle',       // 'idle' | 'loading' | 'result'
+    tempImageUrl: '',
+    foodData: null,
   },
 
   onLoad() {},
 
-  onShow() {
-    // TODO: 拉取今日记录
+  // 点击拍照按钮
+  onCameraTap() {
+    wx.chooseMedia({
+      count: 1,
+      mediaType: ['image'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        const tempImageUrl = res.tempFiles[0].tempFilePath
+        this.setData({ tempImageUrl, state: 'loading' })
+        this._mockAnalyze(tempImageUrl)
+      },
+      fail: () => {
+        // 用户取消，不处理
+      }
+    })
   },
 
-  onCameraTap() {
-    // TODO: 调起相机，识别食物
-    wx.showToast({ title: '拍照功能开发中', icon: 'none' })
-  }
+  // 模拟 AI 识别（mock 数据）
+  _mockAnalyze(imageUrl) {
+    setTimeout(() => {
+      this.setData({
+        foodData: {
+          name: '牛肉便当',
+          calories: 680,
+          protein: 32,
+          fat: 24,
+          carbs: 78,
+          imageUrl,
+        },
+        state: 'result',
+      })
+    }, 1500)
+  },
+
+  // 记录这一餐
+  onSave() {
+    wx.showToast({ title: '已记录', icon: 'success', duration: 1200 })
+    setTimeout(() => {
+      this.setData({ state: 'idle', tempImageUrl: '', foodData: null })
+    }, 1200)
+  },
+
+  // 重新拍摄
+  onRetake() {
+    this.setData({ state: 'idle', tempImageUrl: '', foodData: null })
+  },
 })
